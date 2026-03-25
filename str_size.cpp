cd2f3140cd2c2b11eb58ef68e8d45a8ab3ce3070
joohyeong <ji621638@gmail.com>
2026-03-25 15:20:56 +0900

    newch

---
+++ str_size.cpp
@@ -0,0 +1,27 @@
+//str_size.cpp
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	// 프로그램 설명 하기
+	cout << "Enter all words, then EOF: ";
+	//입력받기
+	string word;
+	string max = "";
+	string min = "";
+	while (cin >> word) {
+		if (min.size() == 0) min = word; //초기화 
+		if (max.size() == 0) max = word; //초기화
+		
+		//크기확인
+		if (word.size() < min.size()) min = word;
+		else if (word.size() > max.size()) max = word;
+
+	}
+	cout <<
+		"Min word: " << min << " size: " << min.size() << endl <<
+		"Max word: " << max << " size: " << max.size() << endl;
+	return 0;
+}
